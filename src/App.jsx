a3b2db162cc039ce4091cfd590eb70b1f0d7eaae
YIS <dtdt203@naver.com>
2024-05-20 15:52:48 +0900

    모르겠습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 export default function App() {
+  const [number, setNumber] = useState(0);
+
+  const onsubmit = (e) => {
+    e.preventDefault();
+
+    if (!number.trim()) {
+      return alert("숫자를 입력하세요");
+    }
+  };
+  const addNumber = () => {
+    setNumber((prev) => prev);
+  };
+
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
-        <button>초기화</button>
+        <form onsubmit={onsubmit}>
+          <input
+            type="Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+          />{" "}
+          만큼을
+          <div>
+            <button onClick={addNumber}>더할게요</button>{" "}
+            <button onClick>뺄게요</button>
+            <button onClick>초기화</button>
+          </div>
+        </form>
       </div>
       <hr />
       <div>
         <h3>결과</h3>
-        <p>0</p>
+        <p>{number}</p>
       </div>
     </div>
   );
